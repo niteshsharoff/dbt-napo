56fba2166a696864b5a5844dfb512bcf78ceefda
Nitesh S <nitesh@growthrunner.com>
2022-03-26 22:39:30 +0000

    added pk

--- models/marts/dim_policy_detail.sql
+++ models/marts/dim_policy_detail.sql
@@ -1,5 +1,5 @@
 with policies as (
-    select p.*, b.* except (pk)
+    select p.*, b.* except (pk),b.pk as breed_pk
     from {{ref('stg_policy_subscription')}} p
     left join {{ref('stg_pet_breed')}} b
     on p.pet = b.pk
--- models/staging/stg_pet_breed.sql
+++ models/staging/stg_pet_breed.sql
@@ -6,19 +6,19 @@
 -- where cast(subscription_active as string) = "true"
 -- ),
 with pet_breeds as (
-select p.pk
-      ,p.breed_category
-      ,p.multipet_number
-      ,p.is_neutered
-      ,p.age_months
-      ,p.size
-      ,p.name
-      ,b.species as breed_species
-      ,b.source as source
-      ,b.name as breed_name 
-from {{ref('raw_pet')}} p
-left join {{ref('raw_breed')}} b
-on p.breed = b.pk
+      select p.pk
+            ,p.breed_category
+            ,p.multipet_number
+            ,p.is_neutered
+            ,p.age_months
+            ,p.size
+            ,p.name
+            ,b.species as breed_species
+            ,b.source as source
+            ,b.name as breed_name 
+      from {{ref('raw_pet')}} p
+      left join {{ref('raw_breed')}} b
+      on p.breed = b.pk
 )
 select * from pet_breeds
 -- final as (
