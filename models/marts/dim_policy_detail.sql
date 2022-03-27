7360617b8d5f3b6c0522b8127f89e44d9e74867b
Nitesh S <nitesh@growthrunner.com>
2022-03-27 00:06:56 +0000

    added customer age feature

--- models/marts/dim_policy_detail.sql
+++ models/marts/dim_policy_detail.sql
@@ -1,6 +1,8 @@
 with policies as (
-    select p.*, b.* except (pk),b.pk as breed_pk
-    from {{ref('stg_policy_subscription')}} p
+    select p.*
+         , b.* except (pk)
+         , b.pk as breed_pk
+    from {{ref('stg_policy_customer')}} p
     left join {{ref('stg_pet_breed')}} b
     on p.pet = b.pk
 )
