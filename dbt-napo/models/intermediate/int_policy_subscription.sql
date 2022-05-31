a35108caf7f2a3aa6b9507abc0055642c6b8697a
Nitesh Sharoff <nitesh@growthrunner.com>
2022-05-31 06:33:22 +0000

    added flag for active policy existed

--- dbt-napo/models/intermediate/int_policy_subscription.sql
+++ dbt-napo/models/intermediate/int_policy_subscription.sql
@@ -9,12 +9,19 @@ subscription_table as ( --latest subscription table
     from grouped_policy_data 
     where row_no = 1
 ),
+active_policy_existed as (
+  select policy
+    ,countif(active=true) as active_policy_existed  
+  from  {{ref('raw_subscription')}}
+  group by policy
+),
 grouped_data as (
     select
          p.pk
         ,p.quote_id
         ,p.reference_number
         ,coalesce(cast(s.active as string),'not set') as subscription_active
+        ,coalesce(if(c.active_policy_existed=1,true,false),false) as active_policy_existed
         ,p.annual_payment_id
         ,p.created_date as created_date
         ,s.created_date as subscription_created_date
@@ -35,5 +42,7 @@ grouped_data as (
     FROM {{ref('raw_policy')}} p
     left join subscription_table s
     on p.pk = s.policy
+    left join active_policy_existed c
+    on c.policy = p.pk
 )
 select * from grouped_data
