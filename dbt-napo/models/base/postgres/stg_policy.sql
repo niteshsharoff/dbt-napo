933f7abe1bad8c88c66a041ae771f12f6db57d21
Nitesh Sharoff <nitesh@growthrunner.com>
2022-09-01 06:50:36 +0000

    _petid field name change in policy JSON

--- dbt-napo/models/base/postgres/stg_policy.sql
+++ dbt-napo/models/base/postgres/stg_policy.sql
@@ -3,5 +3,6 @@ select pk
       ,fields._referencenumber as reference_number
       ,nullif(fields.annual_payment_id,'') as annual_payment_id
       ,nullif(fields.quote_source_reference,'') as quote_source_reference
-      ,fields.* except(policyid, _referencenumber,annual_payment_id,quote_source_reference)
+      ,fields.* except(_petid,policyid, _referencenumber,annual_payment_id,quote_source_reference)
+      ,fields._petid as pet
 from {{source('postgres','policy')}}
