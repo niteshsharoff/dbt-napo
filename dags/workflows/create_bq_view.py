84a8ce26fd244b1a8a8ea39c37c4d9be68ae8728
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2022-11-30 16:18:45 +0000

    Compare the market reporting pipeline (#18)

--- dags/workflows/create_bq_view.py
+++ dags/workflows/create_bq_view.py
@@ -3,6 +3,37 @@ from datetime import datetime
 from google.cloud import bigquery
 
 
+def create_ctm_sales_monthly_view(
+    project_name: str,
+    dataset_name: str,
+    src_table: str,
+    view_name: str,
+    start_date: datetime,
+    end_date: datetime,
+) -> None:
+    try:
+        bq_client = bigquery.Client(project=project_name)
+        dataset_id = "{}.{}".format(project_name, dataset_name)
+        dataset_ref = bq_client.get_dataset(dataset_id)
+        table = bigquery.Table(dataset_ref.table(view_name))
+        table.view_query = """
+            select * except(run_date)
+            from `{project_name}.{dataset_name}.{table_name}` 
+            where date(Transaction_Datetime) >= date('{start_date}', 'UTC')
+            and date(Transaction_Datetime) < date('{end_date}', 'UTC')
+            order by Transaction_Datetime desc
+        """.format(
+            project_name=project_name,
+            dataset_name=dataset_name,
+            table_name=src_table,
+            start_date=start_date,
+            end_date=end_date,
+        )
+        bq_client.create_table(table, exists_ok=True)
+    except Exception:
+        raise
+
+
 def create_napo_benefit_weekly_view(
     project_name: str,
     dataset_name: str,
