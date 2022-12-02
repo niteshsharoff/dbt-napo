d35c9b8b045710fe6426d4e9ab5d8152ef049719
Nitesh Sharoff <nitesh@growthrunner.com>
2022-12-02 08:59:55 +0000

    added weekday and time to academy_bookings

--- dbt-napo/models/marts/academy/dim_academy_bookings.sql
+++ dbt-napo/models/marts/academy/dim_academy_bookings.sql
@@ -1,2 +1,11 @@
-select *
-from {{ref('stg_firestore__bookings')}}
+with raw as (
+      select *
+            ,time(booking_date) as booking_time
+            ,EXTRACT(DAYOFWEEK from booking_date) as booking_weekday
+      from {{ref('stg_firestore__bookings')}}
+)
+select a.* except (booking_weekday)
+      ,b.weekday as booking_weekday
+from raw a
+left join {{ref('lookup_weekday_mapping')}} b
+on a.booking_weekday = b.no
