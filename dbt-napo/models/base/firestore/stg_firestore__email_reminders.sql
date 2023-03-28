5e338a454bda1411f6bd26c5107d4fcba4368109
Nitesh Sharoff <nitesh@becomebusiness.com>
2023-03-28 17:31:50 +0100

    updated id

--- dbt-napo/models/base/firestore/stg_firestore__email_reminders.sql
+++ dbt-napo/models/base/firestore/stg_firestore__email_reminders.sql
@@ -1,6 +1,7 @@
 with raw as (
     select 
-        userId as user_id
+        id
+        ,userId as user_id
         ,message
         ,_imported_at
     from {{source('firestore','email_reminders')}}
