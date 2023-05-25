4570c5d587ed74c4b716e528a869dc48402c6278
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2023-05-25 15:43:53 +0100

    Airflow terraform setup

--- terraform/airflow/modules/airflow/main.tf
+++ terraform/airflow/modules/airflow/main.tf
@@ -12,7 +12,7 @@ resource "google_storage_bucket" "airflow_bucket" {
 
 # Airflow Database
 module "airflow_db" {
-  source = "git@github.com:CharlieNapo/infrastructure.git//terraform/modules/cloud_sql?ref=master"
+  source = "git@github.com:CharlieNapo/infrastructure.git//terraform/modules/cloud_sql?ref=v1.0.0"
 
   project_id  = var.project_id
   owner       = var.owner
@@ -21,8 +21,41 @@ module "airflow_db" {
 
   private_network = var.host_network
 
-  name              = format("%s-airflow-db", var.project_id)
+  name              = "airflow"
   engine            = "POSTGRES_14"
   machine_type      = "db-f1-micro"
   num_read_replicas = 0
+}
+
+# Airflow master node pool
+module "airflow_master_node_pool" {
+  source = "git@github.com:CharlieNapo/infrastructure.git//terraform/modules/gke_node_pool?ref=v1.0.0"
+  region = var.region
+
+  name         = "airflow-master"
+  cluster_name = var.cluster_name
+  project_id   = var.project_id
+
+  node = {
+    machine_type  = var.node_pool.machine_type
+    disk_size     = var.node_pool.disk_size
+    default_count = var.node_pool.default_count
+    min_count     = var.node_pool.min_count
+    max_count     = var.node_pool.max_count
+
+    labels = {
+      cluster = var.cluster_name
+    }
+
+    oauth_scope = [
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/projecthosting",
+      "https://www.googleapis.com/auth/devstorage.full_control",
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/pubsub",
+      "https://www.googleapis.com/auth/bigquery",
+    ]
+  }
 }
--- terraform/airflow/modules/airflow/variables.tf
+++ terraform/airflow/modules/airflow/variables.tf
@@ -19,3 +19,19 @@ variable "host_network" {
   type        = string
   description = "Resource link of the VPC network from which instance is accessible via private IP."
 }
+
+variable "cluster_name" {
+  type        = string
+  description = "The GKE cluster name that the Airflow node pools will be added to"
+}
+
+variable "node_pool" {
+  type = object({
+    machine_type  = string
+    disk_size     = number
+    default_count = number
+    min_count     = number
+    max_count     = number
+  })
+  description = "Node pool configuration"
+}
