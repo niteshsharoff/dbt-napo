9b846b367f4ba907887a22175e7a369f5b9efbc0
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2023-05-31 12:15:57 +0100

    Handle MTA events POC

--- dbt-napo/models/intermediate/int_policy_history.sql
+++ dbt-napo/models/intermediate/int_policy_history.sql
@@ -1,12 +1,37 @@
 with
-    policy as (select * from {{ ref("stg_raw__policy_ledger") }}),
-    customer as (select * from {{ ref("stg_raw__customer_ledger") }}),
-    pet as (select * from {{ ref("stg_raw__pet_ledger") }}),
-    user as (select * from {{ source("raw", "user") }}),
-    product as (select * from {{ source("raw", "product") }}),
-    breed as (select * from {{ source("raw", "breed") }} where run_date = parse_date('%Y-%m-%d', '{{run_started_at.date()}}')),
-    quote as (select * from {{ source("raw", "quoterequest") }}),
-    discount as (select * from {{ ref('stg_raw__vouchercode') }}),
+    policy as (
+        select * 
+        from {{ ref("stg_raw__policy_ledger") }}
+    ),
+    customer as (
+        select * 
+        from {{ ref("stg_raw__customer_ledger") }}
+    ),
+    pet as (
+        select * 
+        from {{ ref("stg_raw__pet_ledger") }}
+    ),
+    user as (
+        select * 
+        from {{ source("raw", "user") }}
+    ),
+    product as (
+        select * 
+        from {{ source("raw", "product") }}
+    ),
+    breed as (
+        select *
+        from {{ source("raw", "breed") }}
+        where run_date = parse_date('%Y-%m-%d', '{{run_started_at.date()}}')
+    ),
+    quote as (
+        select 
+            quote_request_id, 
+            msm_sales_tracking_urn, 
+            created_at
+        from {{ source("raw", "quoterequest") }}
+    ),
+    discount as (select * from {{ ref("stg_raw__vouchercode") }}),
     joint_history as (
         select
             policy,
@@ -19,8 +44,12 @@ with
                 select
                     policy,
                     customer,
-                    greatest(customer.effective_from, policy.effective_from) as row_effective_from,
-                    least(customer.effective_to, policy.effective_to) as row_effective_to
+                    greatest(
+                        customer.effective_from, policy.effective_from
+                    ) as row_effective_from,
+                    least(
+                        customer.effective_to, policy.effective_to
+                    ) as row_effective_to
                 from policy
                 left join
                     customer
@@ -33,18 +62,53 @@ with
             on policy.pet_id = pet.pet_id
             and pet.effective_to >= row_effective_from
             and pet.effective_from < row_effective_to
+        where policy.reference_number = 'ESS-BIN-0011'
+    ),
+    joint_history_with_change_audit as (
+        select
+            *,
+            struct(
+                {% for tuple in [
+                    ["policy", "annual_price"], 
+                    ["policy", "pet_id"],
+                    ["policy", "customer_id"],
+                    ["policy", "start_date"],
+                    ["policy", "end_date"],
+                    ["policy", "cancel_date"],
+                    ["customer", "postal_code"],
+                    ["pet", "name"],
+                    ["pet", "breed_category"],
+                    ["pet", "date_of_birth"],
+                ] -%}
+                {% set model = tuple[0] -%}
+                {% set column = tuple[1] -%}
+                case
+                    when {{ model }}.{{ column }} != lag({{ model }}.{{ column }}) over (partition by policy.policy_id order by row_effective_from)
+                    or (
+                        {{ model }}.{{ column }} is not null 
+                        and lag({{ model }}.{{ column }}) over (partition by policy.policy_id order by row_effective_from) is null
+                    )
+                    then true
+                    else false
+                end as {{ model }}_{{ column }}_changed
+                {%- if not loop.last %}, {% endif %}
+                {% endfor %}
+            ) as mta
+        from joint_history
     )
-select quote
-    , policy
-    , product
-    , customer
-    , user
-    , pet
-    , breed
-    , discount
-    , row_effective_from
-    , row_effective_to
-from joint_history j
+select
+    quote,
+    policy,
+    product,
+    customer,
+    user,
+    pet,
+    breed,
+    discount,
+    mta,
+    row_effective_from,
+    row_effective_to,
+from joint_history_with_change_audit j
 left join user on j.customer.user_id = user.id
 left join product on j.policy.product_id = product.id
 left join breed on j.pet.breed_id = breed.id
--- dbt-napo/models/intermediate/int_policy_transaction.sql
+++ dbt-napo/models/intermediate/int_policy_transaction.sql
@@ -10,6 +10,23 @@ with
         from policy_history
         where row_effective_from = policy.cancelled_at and policy.reinstated_at is null
     ),
+    mtas as (
+        select 'MTA' as transaction_type, row_effective_from as transaction_at, *
+        from policy_history
+        where (
+            row_effective_from != policy.sold_at
+            and (row_effective_from != policy.cancelled_at or policy.cancelled_at is null)
+            and (
+                _mta.policy_annual_price_changed
+                or _mta.policy_start_date_changed
+                or _mta.policy_end_date_changed
+                or _mta.customer_postal_code_changed
+                or _mta.pet_name_changed
+                or _mta.pet_breed_category_changed
+                or _mta.pet_date_of_birth_changed
+            )
+        )
+    ),
     all_transactions as (
         select * from sold_policies
         union all
