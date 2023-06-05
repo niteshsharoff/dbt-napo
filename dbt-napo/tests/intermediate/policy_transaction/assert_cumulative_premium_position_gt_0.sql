c2589133944090040974d442576dd6da3b401399
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2023-06-05 11:56:44 +0100

    Add comment

--- dbt-napo/tests/intermediate/policy_transaction/assert_cumulative_premium_position_gt_0.sql
+++ dbt-napo/tests/intermediate/policy_transaction/assert_cumulative_premium_position_gt_0.sql
@@ -1,3 +1,13 @@
+/* 
+  GIVEN 
+    a table of policy transactions
+    
+  WHEN 
+    we sum up the premium difference for each policy
+  
+  THEN 
+    we expect the cumulative premium position to be >= 0
+*/
 select policy.reference_number
   , sum(premium_difference) as cumulative_premium_position
 from {{ref('int_underwriter__policy_transaction')}}
