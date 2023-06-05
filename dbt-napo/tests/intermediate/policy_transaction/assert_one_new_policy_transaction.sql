3cb2d62203bfa029308fee05eb4ab59e764d399e
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2023-06-05 17:00:42 +0100

    Additional test case

---
+++ dbt-napo/tests/intermediate/policy_transaction/assert_one_new_policy_transaction.sql
@@ -0,0 +1,14 @@
+/* 
+  GIVEN 
+    a table of policy transactions
+    
+  WHEN 
+    we count the number of new policies or renewal transactions
+  
+  THEN 
+    we expect the row count to be <= 1
+*/
+select policy.reference_number, transaction_type, count(*) as row_count
+from {{ref('int_underwriter__policy_transaction')}}
+group by policy.reference_number, transaction_type
+having (transaction_type = 'New Policy' or transaction_type = 'Renewal') and row_count <= 1
