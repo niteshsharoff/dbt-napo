dd6b61a423119d328821ac6f04f168bfd5badba8
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2023-06-08 16:45:01 +0100

    Move dimension pruning to transaction table

--- dbt-napo/models/intermediate/int_policy_history.sql
+++ dbt-napo/models/intermediate/int_policy_history.sql
@@ -1,146 +1,31 @@
-{% set MTA_FIELDS = [
-    ["policy", "annual_price"],
-    ["policy", "payment_plan_type"],
-    ["policy", "accident_cover_start_date"],
-    ["policy", "illness_cover_start_date"],
-    ["policy", "start_date"],
-    ["policy", "end_date"],
-    ["policy", "cancel_date"],
-    ["customer", "first_name"],
-    ["customer", "last_name"],
-    ["customer", "email"],
-    ["customer", "date_of_birth"],
-    ["customer", "postal_code"],
-    ["pet", "name"],
-    ["pet", "date_of_birth"],
-    ["pet", "gender"],
-    ["pet", "size"],
-    ["pet", "cost"],
-    ["pet", "is_neutered"],
-    ["pet", "is_microchipped"],
-    ["pet", "is_vaccinated"],
-    ["pet", "species"],
-    ["pet", "breed_category"],
-    ["pet", "breed_name"],
-    ["pet", "breed_source"],
-    ["pet", "has_pre_existing_conditions"]
-] %}
-
 with
     policy as (
-        select policy.policy_id
-            , policy.quote_id
-            , policy.product_id
-            , policy.customer_id
-            , policy.pet_id
-            , policy.voucher_code_id as voucher_id
-            , original_policy.original_policy_id
-            , policy.uuid
-            , policy.reference_number
-            , policy.quote_source
-            , original_policy.original_quote_source
-            , original_policy.current_policy_year as policy_year
-            , policy.payment_plan_type
-            , policy.annual_payment_id
-            , policy.annual_price
-            , policy.notes
-            , policy.accident_cover_start_date
-            , policy.illness_cover_start_date
-            , policy.created_date
-            , policy.start_date
-            , policy.end_date
-            , policy.cancel_date
-            , policy.cancel_detail
-            , cancel_mapping.cancel_reason
-            , policy.sold_at
-            , policy.cancelled_at
-            , policy.reinstated_at
-            , policy.change_reason
-            , policy.effective_from
-            , policy.effective_to
+        select * except(policy_id, cancel_reason, run_date), policy.policy_id, cancel_mapping.cancel_reason
         from {{ ref("stg_raw__policy_ledger") }} policy
         left join {{ ref("lookup_policy_cancel_reason") }} cancel_mapping 
             on policy.cancel_reason = cancel_mapping.id
         left join {{ ref("int_original_policy") }} original_policy
             on policy.policy_id = original_policy.policy_id
     ),
-    product as (
-        select id as product_id
-            , reference
-            , name
-            , vet_fee_cover
-            , complementary_treatment_cover
-            , dental_cover
-            , emergency_boarding_cover
-            , third_person_liability_excess
-            , third_person_liability_cover
-            , pet_death_cover
-            , travel_cover
-            , missing_pet_cover
-            , behavioural_treatment_cover
-            , co_pay
-            , excess
-        from {{ source("raw", "product") }}
-    ),
+    product as (select * from {{ source("raw", "product") }}),
     customer as (
-        select customer.customer_id
-            , customer.uuid
-            , user.first_name
-            , user.last_name
-            , user.email
-            , customer.street_address
-            , customer.address_locality
-            , customer.address_region
-            , customer.postal_code
-            , customer.date_of_birth
-            , customer.change_reason
-            , customer.effective_from
-            , customer.effective_to
+        select * except(run_date), customer.run_date
         from {{ ref("stg_raw__customer_ledger") }} customer
         left join {{ source("raw", "user") }} user on customer.user_id = user.id
     ),
     pet as (
-        select pet.pet_id
-            , pet.uuid
+        select * except(run_date, name, species, source)
+            , pet.run_date
             , pet.name
-            , pet.date_of_birth
-            , case 
-                when pet.gender = '1' then 'male'
-                when pet.gender = '2' then 'female'
-                else null
-            end as gender
-            , pet.size
-            , pet.cost
-            , pet.is_neutered
-            , pet.is_microchipped
-            , pet.is_vaccinated
             , pet.species
-            , pet.breed_category
             , breed.name as breed_name
             , breed.source as breed_source
-            , pet.has_pre_existing_conditions
-            , pet.change_reason
-            , pet.multipet_number
-            , pet.effective_from
-            , pet.effective_to
         from {{ ref("stg_raw__pet_ledger") }} pet
         left join {{ source("raw", "breed") }} breed 
             on pet.breed_id = breed.id and breed.run_date = parse_date('%Y-%m-%d', '{{run_started_at.date()}}')
     ),
-    quote as (
-        select quote_id
-        , pricing_model_version
-        , msm_sales_tracking_urn
-        , timestamp_millis(created_at) as created_at
-        from {{ ref("int_quote") }}
-    ),
-    discount as (
-        select voucher_id
-            , voucher_code
-            , discount_percentage
-            , affiliate_channel
-        from {{ ref("stg_raw__vouchercode") }}
-    ),
+    quote as (select * from {{ ref("int_policy_quote") }}),
+    discount as (select * from {{ ref("stg_raw__vouchercode") }}),
     joint_history as (
         select
             policy,
@@ -167,49 +52,17 @@ with
             on policy.pet_id = pet.pet_id
             and pet.effective_to >= row_effective_from
             and pet.effective_from < row_effective_to
-    ),
-    joint_history_with_audit as (
-        select
-            *
-            , struct(
-                {% for mta_field in MTA_FIELDS -%}
-                {% set model = mta_field[0] -%}
-                {% set column = mta_field[1] -%}
-                case
-                    when {{ model }}.{{ column }} 
-                    != lag({{ model }}.{{ column }}) over (partition by policy.policy_id order by row_effective_from)
-                    or (
-                        {{ model }}.{{ column }} is not null 
-                        and lag({{ model }}.{{ column }}) over (partition by policy.policy_id order by row_effective_from) is null
-                    )
-                    then true
-                    else false
-                end as {{ model }}_{{ column }}_changed
-                {%- if not loop.last %}, {% endif %}
-                {% endfor %}
-                , policy.change_reason as policy_row_change_reason
-                , customer.change_reason as customer_row_change_reason
-                , pet.change_reason as pet_row_change_reason
-                , policy.effective_from as policy_row_effective_from
-                , policy.effective_to as policy_row_effective_to
-                , customer.effective_from as customer_row_effective_from
-                , customer.effective_to as customer_row_effective_to
-                , pet.effective_from as pet_row_effective_from
-                , pet.effective_to as pet_row_effective_to
-            ) as _audit
-        from joint_history
     )
 select
     quote,
-    (select as struct policy.* except(change_reason, effective_from, effective_to)) as policy,
-    (select as struct customer.* except(change_reason, effective_from, effective_to)) as customer,
-    (select as struct pet.* except(change_reason, effective_from, effective_to)) as pet,
+    policy,
+    customer,
+    pet,
     product,
     discount,
-    _audit,
     row_effective_from,
     row_effective_to,
-from joint_history_with_audit j
-left join product on j.policy.product_id = product.product_id
+from joint_history j
+left join product on j.policy.product_id = product.id
 left join quote on j.policy.quote_id = quote.quote_id
-left join discount on j.policy.voucher_id = discount.voucher_id
+left join discount on j.policy.voucher_code_id = discount.voucher_id
--- dbt-napo/models/intermediate/int_policy_transaction.sql
+++ dbt-napo/models/intermediate/int_policy_transaction.sql
@@ -28,55 +28,196 @@
 
 with
     policy_history as (
-        select * from {{ ref("int_policy_history") }}
+        select 
+        row_effective_from
+        , row_effective_to
+        , struct(
+            quote.quote_id
+            , quote.pricing_model_version
+            , quote.msm_sales_tracking_urn
+            , quote.created_at
+        ) as quote
+        , struct(
+            policy.policy_id
+            , policy.quote_id
+            , policy.product_id
+            , policy.customer_id
+            , policy.pet_id
+            , policy.voucher_code_id as voucher_id
+            , policy.original_policy_id
+            , policy.uuid
+            , policy.reference_number
+            , policy.quote_source
+            , policy.original_quote_source
+            , policy.current_policy_year as policy_year
+            , policy.payment_plan_type
+            , policy.annual_payment_id
+            , policy.annual_price
+            , policy.notes
+            , policy.accident_cover_start_date
+            , policy.illness_cover_start_date
+            , policy.created_date
+            , policy.start_date
+            , policy.end_date
+            , policy.cancel_date
+            , policy.cancel_detail
+            , policy.cancel_reason
+            , policy.sold_at
+            , policy.cancelled_at
+            , policy.reinstated_at
+            , policy.change_reason
+            , policy.effective_from
+            , policy.effective_to
+        ) as policy
+        , struct(
+            customer.customer_id
+            , customer.uuid
+            , customer.first_name
+            , customer.last_name
+            , customer.email
+            , customer.street_address
+            , customer.address_locality
+            , customer.address_region
+            , customer.postal_code
+            , customer.date_of_birth
+            , customer.change_reason
+            , customer.effective_from
+            , customer.effective_to
+        ) as customer
+        , struct(
+            pet.pet_id
+            , pet.uuid
+            , pet.name
+            , pet.date_of_birth
+            , case 
+                when pet.gender = '1' then 'male'
+                when pet.gender = '2' then 'female'
+                else null
+            end as gender
+            , pet.size
+            , pet.cost
+            , pet.is_neutered
+            , pet.is_microchipped
+            , pet.is_vaccinated
+            , pet.species
+            , pet.breed_category
+            , pet.breed_name
+            , pet.breed_source
+            , pet.has_pre_existing_conditions
+            , pet.change_reason
+            , pet.multipet_number
+            , pet.effective_from
+            , pet.effective_to
+        ) as pet
+        , struct (
+            product.id as product_id
+            , product.reference
+            , product.name
+            , product.vet_fee_cover
+            , product.complementary_treatment_cover
+            , product.dental_cover
+            , product.emergency_boarding_cover
+            , product.third_person_liability_excess
+            , product.third_person_liability_cover
+            , product.pet_death_cover
+            , product.travel_cover
+            , product.missing_pet_cover
+            , product.behavioural_treatment_cover
+            , product.co_pay
+            , product.excess
+        ) as product
+        , struct (
+            discount.voucher_id
+            , discount.voucher_code
+            , discount.discount_percentage
+            , discount.affiliate_channel
+        ) as discount
+        from {{ ref("int_policy_history") }}
+    ),
+    policy_history_with_audit_dimension as (
+        select quote
+            , (select as struct policy.* except(change_reason, effective_from, effective_to)) as policy
+            , (select as struct customer.* except(change_reason, effective_from, effective_to)) as customer
+            , (select as struct pet.* except(change_reason, effective_from, effective_to)) as pet
+            , product
+            , discount
+            , struct(
+                {% for mta_field in MTA_FIELDS -%}
+                {% set model = mta_field[0] -%}
+                {% set column = mta_field[1] -%}
+                case
+                    when {{ model }}.{{ column }} 
+                    != lag({{ model }}.{{ column }}) over (partition by policy.policy_id order by row_effective_from)
+                    or (
+                        {{ model }}.{{ column }} is not null 
+                        and lag({{ model }}.{{ column }}) over (partition by policy.policy_id order by row_effective_from) is null
+                    )
+                    then true
+                    else false
+                end as {{ model }}_{{ column }}_changed
+                {%- if not loop.last %}, {% endif %}
+                {% endfor %}
+                , policy.change_reason as policy_row_change_reason
+                , customer.change_reason as customer_row_change_reason
+                , pet.change_reason as pet_row_change_reason
+                , policy.effective_from as policy_row_effective_from
+                , policy.effective_to as policy_row_effective_to
+                , customer.effective_from as customer_row_effective_from
+                , customer.effective_to as customer_row_effective_to
+                , pet.effective_from as pet_row_effective_from
+                , pet.effective_to as pet_row_effective_to
+            ) as _audit
+            , row_effective_from
+            , row_effective_to
+        from policy_history
     ),
     new_policies as (
         select 'New Policy' as transaction_type, row_effective_from as transaction_at, *
-        from policy_history r
+        from policy_history_with_audit_dimension p
         where policy.quote_source != 'renewal' 
             and row_effective_from = (
                 select min(policy.sold_at)
-                from policy_history
-                where policy.policy_id = r.policy.policy_id 
+                from policy_history_with_audit_dimension
+                where policy.policy_id = p.policy.policy_id 
             )
     ),
     ntus as (
         select 'NTU' as transaction_type, row_effective_from as transaction_at, *
-        from policy_history
+        from policy_history_with_audit_dimension
         where row_effective_from = policy.cancelled_at
             and policy.reinstated_at is null
             and date_diff(policy.cancel_date, policy.start_date, day) <= 14
     ),
     new_cancellations as (
         select 'Cancellation' as transaction_type, row_effective_from as transaction_at, *
-        from policy_history
+        from policy_history_with_audit_dimension
         where row_effective_from = policy.cancelled_at 
             and policy.reinstated_at is null
             and date_diff(policy.cancel_date, policy.start_date, day) > 14
     ),
     renewals as (
         select 'Renewal' as transaction_type, row_effective_from as transaction_at, *
-        from policy_history r
+        from policy_history_with_audit_dimension r
         where policy.quote_source = 'renewal'
             and row_effective_from = (
                 select min(policy.sold_at)
-                from policy_history
+                from policy_history_with_audit_dimension
                 where policy.policy_id = r.policy.policy_id 
             )
     ),
     reinstatements as (
         select 'Reinstatement' as transaction_type, row_effective_from as transaction_at, *
-        from policy_history
+        from policy_history_with_audit_dimension
         where row_effective_from = policy.reinstated_at
     ),
     reinstated_cancellations as (
         select 'Cancellation' as transaction_type, row_effective_from as transaction_at, *
-        from policy_history
+        from policy_history_with_audit_dimension
         where row_effective_from = policy.cancelled_at and policy.reinstated_at is not null
     ),
     all_mtas as (
         select *
-        from policy_history
+        from policy_history_with_audit_dimension
         where (
             row_effective_from != policy.sold_at
             and (row_effective_from != policy.cancelled_at or policy.cancelled_at is null)
@@ -119,7 +260,7 @@ with
             quote,
             (
                 select as struct policy.* except(
-                    quote_id, 
+                    quote_id,
                     product_id, 
                     customer_id, 
                     pet_id, 
@@ -129,8 +270,8 @@ with
             customer,
             pet,
             product,
-            discount,
-            _audit
+            discount
+            -- _audit
         from all_transactions
         order by transaction_at
     )
