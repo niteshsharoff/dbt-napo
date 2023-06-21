4d1fe8fce627f6e86bdc74c14ede269b4402efd3
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2023-06-21 12:18:23 +0100

    Move GKE cluster into Airflow module

---
+++ terraform/airflow/ae32-vpcservice-dev/main.tf
@@ -0,0 +1,54 @@
+data "google_storage_bucket_object_content" "shared_vpc_network_manifest" {
+  bucket = "ae32-vpcservice-prod-terraform-bucket"
+  name   = "core-infrastructure/ae32-vpc-host/networking"
+}
+
+locals {
+  shared_vpc  = jsondecode(base64decode(data.google_storage_bucket_object_content.shared_vpc_network_manifest.content))
+  project_id  = "ae32-vpcservice-dev"
+  region      = "europe-west2"
+  environment = "development"
+  cluster = {
+    name                 = "data"
+    master_ip_cidr_range = "172.19.0.16/28"
+  }
+}
+
+module "airflow" {
+  source      = "../modules/airflow"
+  region      = local.region
+  environment = local.environment
+
+  bastion_host_ip = local.shared_vpc["bastion_host"]["ip"]
+
+  host_project = {
+    id      = local.shared_vpc["host_project_id"]
+    network = local.shared_vpc["host_network"]
+  }
+
+  service_project = {
+    id     = local.project_id
+    subnet = local.shared_vpc["development_subnet"]["self_link"]
+  }
+
+  db_config = {
+    name         = "airflow"
+    engine       = "POSTGRES_14"
+    machine_type = "db-f1-micro"
+  }
+
+  cluster_config = {
+    name                 = local.cluster.name
+    master_ip_cidr_range = local.cluster.master_ip_cidr_range
+    pod_ip_cidr_range    = local.shared_vpc["development_subnet"]["pod_ip_cidr_range_name"]
+    svc_ip_cidr_range    = local.shared_vpc["development_subnet"]["svc_ip_cidr_range_name"]
+  }
+
+  master_node_pool = {
+    machine_type  = "n2-standard-4"
+    disk_size     = 40
+    default_count = 1
+    min_count     = 1
+    max_count     = 1
+  }
+}
--- terraform/airflow/modules/airflow/main.tf
+++ terraform/airflow/modules/airflow/main.tf
@@ -1,7 +1,7 @@
 # Airflow remote logging bucket
 resource "google_storage_bucket" "airflow_bucket" {
-  name          = format("%s-airflow", var.project_id)
-  project       = var.project_id
+  name          = format("%s-airflow", var.service_project.id)
+  project       = var.service_project.id
   location      = local.location
   force_destroy = false
 
@@ -18,17 +18,48 @@ locals {
 module "airflow_db" {
   source = "git@github.com:CharlieNapo/infrastructure.git//terraform/modules/cloud_sql?ref=v1.0.0"
 
-  project_id  = var.project_id
+  project_id  = var.service_project.id
   owner       = var.owner
   region      = var.region
-  environment = "production"
+  environment = var.environment
 
-  private_network = var.host_network
+  private_network = var.host_project.network
 
-  name              = "airflow"
-  engine            = "POSTGRES_14"
-  machine_type      = "db-f1-micro"
+  name              = var.db_config.name
+  engine            = var.db_config.engine
+  machine_type      = var.db_config.machine_type
   num_read_replicas = 0
+}
+
+# Airflow GKE cluster
+module "gke_cluster" {
+  source = "git@github.com:CharlieNapo/infrastructure.git//terraform/modules/gke_cluster?ref=v1.0.0"
+  region = var.region
+
+  # Update for multi node deployment
+  node_locations = [
+    format("%s-a", var.region)
+  ]
+
+  host_project = {
+    id      = var.host_project.id
+    network = var.host_project.network
+  }
+
+  service_project = {
+    id     = var.service_project.id
+    subnet = var.service_project.subnet
+  }
+
+  cluster = {
+    name                 = var.cluster_config.name
+    master_ip_cidr_range = var.cluster_config.master_ip_cidr_range
+    pod_ip_cidr_range    = var.cluster_config.pod_ip_cidr_range
+    svc_ip_cidr_range    = var.cluster_config.svc_ip_cidr_range
+    enable_dataplane_v2  = true
+  }
+
+  bastion_ip_cidr_range = format("%s/32", var.bastion_host_ip)
 }
 
 # Airflow master node pool
@@ -37,18 +68,18 @@ module "airflow_master_node_pool" {
   region = var.region
 
   name         = "airflow-master"
-  cluster_name = var.cluster_name
-  project_id   = var.project_id
+  cluster_name = var.cluster_config.name
+  project_id   = var.service_project.id
 
   node = {
-    machine_type  = var.node_pool.machine_type
-    disk_size     = var.node_pool.disk_size
-    default_count = var.node_pool.default_count
-    min_count     = var.node_pool.min_count
-    max_count     = var.node_pool.max_count
+    machine_type  = var.master_node_pool.machine_type
+    disk_size     = var.master_node_pool.disk_size
+    default_count = var.master_node_pool.default_count
+    min_count     = var.master_node_pool.min_count
+    max_count     = var.master_node_pool.max_count
 
     labels = {
-      cluster = var.cluster_name
+      cluster = var.cluster_config.name
     }
 
     oauth_scope = [
