36c02ffb365520d339183d45c9aa70c661412501
Nitesh Sharoff <nitesh@growthrunner.com>
2023-06-28 11:50:02 +0000

    added active puppy & adolescent policies as per Kaveh's request

--- dbt-napo/models/marts/metrics/fct_active_policies.sql
+++ dbt-napo/models/marts/metrics/fct_active_policies.sql
@@ -8,6 +8,8 @@ select
       ,format_date('%V',current_date()) as week
       ,format_date('%A',current_date()) as weekday
       ,count(*) as active_policies
+      ,countif(pet_current_age_months<=5) as active_puppy_policies
+      ,countif(pet_current_age_months>=6 and pet_current_age_months<=24) as active_adolescent_policies
 from {{ref('dim_policy_detail')}}
 where is_policy_expired is false
 and (is_subscription_active is not null or annual_payment_id is not null)
