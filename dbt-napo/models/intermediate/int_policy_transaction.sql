0bad1b04636f281efae2aaf5243b859a38f8dbc0
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2023-07-04 16:47:53 +0100

    Test pre-commit
    Revert "Test pre-commit"
    
    This reverts commit 45fb4223d881e8398ffe86549c8b88a0ba8b5902.
    
    Test pre-commit
    
    Test pre-commit
    
    Update documentation with pre-commit hook

--- dbt-napo/models/intermediate/int_policy_transaction.sql
+++ dbt-napo/models/intermediate/int_policy_transaction.sql
@@ -6,14 +6,18 @@ with
         where row_effective_from = policy.sold_at and policy.quote_source != 'renewal'
     ),
     first_time_cancellations as (
-        select 'Cancel Policy' as transaction_type, row_effective_from as transaction_at, *
+        select
+            'Cancel Policy' as transaction_type, row_effective_from as transaction_at, *
         from policy_history
         where row_effective_from = policy.cancelled_at and policy.reinstated_at is null
     ),
     all_transactions as (
-        select * from sold_policies
+        select *
+        from sold_policies
         union all
-        select * from first_time_cancellations
+        select *
+        from first_time_cancellations
     )
-select transaction_type, transaction_at, quote, policy, product, customer, user, pet, breed
+select
+    transaction_type, transaction_at, quote, policy, product, customer, user, pet, breed
 from all_transactions t
