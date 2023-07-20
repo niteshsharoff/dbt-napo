e78eec8e4161d564d8945e790595276544532583
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2023-07-20 14:32:10 +0100

    Migrate GoCompare sales report to BigQuery (#95)

---
+++ airflow/dags/sql/gocompare_sales_report.sql
@@ -0,0 +1,3 @@
+select * except (_transaction_at)
+from dbt_marts.gocompare_cumulative_sales_report
+where _transaction_at >= '{{ start_date }}' and _transaction_at < '{{ end_date }}'
--- dbt-napo/models/intermediate/int_policy_transaction.sql
+++ dbt-napo/models/intermediate/int_policy_transaction.sql
@@ -54,6 +54,7 @@ with
                 policy.reference_number,
                 policy.quote_source,
                 policy.original_quote_source,
+                policy.quote_source_reference,
                 ifnull(policy.current_policy_year, 0) as policy_year,
                 policy.payment_plan_type,
                 policy.annual_payment_id,
