fc6c0026045d17b8bf6702876ecfaea7d290cb63
Tim Halford <tim+2@napo.pet>
2023-09-21 15:22:13 +0100

    Use DBT mart in CTM pipeline

--- airflow/dags/workflows/create_bq_view.py
+++ airflow/dags/workflows/create_bq_view.py
@@ -36,36 +36,6 @@ def create_pcw_sales_view(
     bq_client.create_table(table, exists_ok=True)
 
 
-def create_ctm_sales_monthly_view(
-    project_name: str,
-    dataset_name: str,
-    src_table: str,
-    view_name: str,
-    start_date: datetime,
-    end_date: datetime,
-) -> None:
-    bq_client = bigquery.Client(project=project_name)
-    dataset_id = "{}.{}".format(project_name, dataset_name)
-    dataset_ref = bq_client.get_dataset(dataset_id)
-    table = bigquery.Table(dataset_ref.table(view_name))
-    table.view_query = """
-        select * except(run_date)
-        from `{project_name}.{dataset_name}.{table_name}` 
-        where parse_datetime('{format}', {column}) >= date('{start_date}', 'UTC')
-        and parse_datetime('{format}', {column}) < date('{end_date}', 'UTC')
-        order by Transaction_Datetime desc
-    """.format(
-        project_name=project_name,
-        dataset_name=dataset_name,
-        table_name=src_table,
-        start_date=start_date,
-        end_date=end_date,
-        column="Transaction_Datetime",
-        format="%d/%m/%Y %H:%M:%S",
-    )
-    bq_client.create_table(table, exists_ok=True)
-
-
 def create_bq_view(
     project_name: str,
     dataset_name: str,
