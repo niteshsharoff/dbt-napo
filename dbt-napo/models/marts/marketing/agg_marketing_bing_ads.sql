cf3811c1ad1ee08954e7eb98bfb61cb9fdede015
Nitesh Sharoff <nitesh@growthrunner.com>
2023-11-27 22:00:57 +0000

    bing ads - provisional

---
+++ dbt-napo/models/marts/marketing/agg_marketing_bing_ads.sql
@@ -0,0 +1,11 @@
+SELECT 
+     timeperiod as date
+    ,sum(impressions) as impressions
+    ,sum(clicks) as clicks
+    ,sum(spend) as cost
+    ,sum(conversions) as conversions
+    ,max(currencycode) as currency
+    ,sum(revenue) as conversion_value
+FROM {{ref('stg_src_airbyte__bing_account_performance_report_daily')}} 
+group by 1
+order by 1
