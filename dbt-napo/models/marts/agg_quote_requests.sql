c3744f1dbc29034171abfa4bebf9140796ed583d
Nitesh Sharoff <nitesh@growthrunner.com>
2023-11-29 18:05:58 +0000

    removal of incremental strategy - agg_quote_requests

--- dbt-napo/models/marts/agg_quote_requests.sql
+++ dbt-napo/models/marts/agg_quote_requests.sql
@@ -1,6 +1,5 @@
 {{
     config(
-        materialized="incremental",
         partition_by={
             'field':'created_date',
             'data_type':'date',
@@ -9,10 +8,11 @@
     )
 }}
 
+with data as (
 select cast((a.created_at) as date) as created_date
         ,case(a.source)
             when 'direct' then 'direct'
-            else lower(b.pcw_name)
+            else pcw_name
          end as quote_source
         ,count(quote_id) as total_offered_quotes 
 from {{ref('fct_quote_requests')}} a
@@ -21,3 +21,6 @@ on a.source = b.quote_code_name
 where state = 'offered'
 and a.source != 'renewal'
 group by 1,2
+)
+select * replace(lower(quote_source) as quote_source)
+from data
