44a5e1ff6dbc5d738c91b2cc05eaa113f6f8156e
Nitesh Sharoff <nitesh@growthrunner.com>
2023-11-29 18:16:40 +0000

    agg_sold_policies_excl_referrals

---
+++ dbt-napo/models/marts/agg_sold_policies_excl_renewal.sql
@@ -0,0 +1,21 @@
+{{config(
+    partition_by={
+        'field':'created_date',
+        'data_type':'date',
+        'granularity':'day'
+    },
+    cluster_by=['quote_source']
+)}}
+
+with data as (
+select created_date
+      ,quote_source
+      ,count(policy_id) as total_sold_policies
+from {{ref('dim_policy_detail')}}
+where (is_subscription_active IS NOT NULL
+      OR annual_payment_id IS NOT NULL) 
+      AND quote_source != 'renewal'
+group by 1,2
+)
+select * 
+from data
