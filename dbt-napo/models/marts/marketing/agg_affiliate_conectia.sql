0cd23b93b8c7d8532cdaaca18a5ed42cde79d7a0
Nitesh Sharoff <nitesh@growthrunner.com>
2023-11-30 05:23:38 +0000

    conectia - modified epc to cpc as it's calculated the same way

--- dbt-napo/models/marts/marketing/agg_affiliate_conectia.sql
+++ dbt-napo/models/marts/marketing/agg_affiliate_conectia.sql
@@ -3,7 +3,7 @@ select cast(timestamp_seconds(cast(date_dt as int64)) as date) as date
       ,sum(impressions) as impressions
       ,sum(conversions) as conversions
       ,sum(revenue) as cost_gbp
-      ,avg(epc) as epc
+      ,avg(epc) as cpc
       ,avg(conversion_rate) as conv_rate
 from {{ref('stg_src_airbyte__conectia_daily_summary')}}
 group by 1
