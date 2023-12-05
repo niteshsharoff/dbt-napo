d18f147380b75c825f0950d729cf0eddec98955a
Nitesh Sharoff <nitesh@growthrunner.com>
2023-12-05 15:34:47 +0000

    Added lead actions to facebook and leadgen, growth categorisation

--- dbt-napo/models/marts/growth/marketing_affiliates/agg_marketing_facebook_ads_by_campaign_type.sql
+++ dbt-napo/models/marts/growth/marketing_affiliates/agg_marketing_facebook_ads_by_campaign_type.sql
@@ -12,6 +12,11 @@
 with data as (
 select
        date_start as date
+    ,case 
+        when lower(campaign_name) like '%leadgen%' then 'leadgen'
+        when lower(campaign_name) not like any ('%leadgen%','%standalone%') then 'growth'
+        else 'other'
+    end as napo_campaign_type
       ,sum(reach) as reach --TO DO resolve this to match Asights report
       ,sum(impressions) as impressions
       ,sum(clicks) as clicks
@@ -27,7 +32,7 @@ select
       ,sum(cast((select json_extract_scalar(c,'$.value') from unnest(json_extract_array(conversion_values,'$')) c where json_extract_scalar(c,'$.action_type')='offsite_conversion.fb_pixel_custom.purchase_insurance') as numeric)) as purchase_insurance_value
       ,sum(cast((select json_extract_scalar(c,'$.value') from unnest(json_extract_array(actions,'$')) c where json_extract_scalar(c,'$.action_type')='lead') as numeric)) as lead_actions
 from {{ref('stg_src_airbyte__facebook_ads_insights')}}
-group by 1
+group by 1,2
 )
 select * 
 from data
