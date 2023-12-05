71fa34f3d028f0a1cb02fcd9682328afafa8349d
Nitesh Sharoff <nitesh@growthrunner.com>
2023-12-05 17:10:37 +0000

    G Ads - Added youtube filter

--- dbt-napo/models/marts/growth/marketing_affiliates/agg_marketing_google_ads_by_campaign_type.sql
+++ dbt-napo/models/marts/growth/marketing_affiliates/agg_marketing_google_ads_by_campaign_type.sql
@@ -18,10 +18,14 @@ SELECT
         when lower(campaign_name) not like any ('%leadgen%','%standalone%') then 'growth'
         else 'other'
     end as napo_campaign_type
+    ,case
+    when lower(segments_ad_network_type) like '%youtube%' then true
+    else false
+    end as is_youtube_campaign
   ,sum(metrics_impressions) as impressions
   ,sum(metrics_clicks) as clicks 
   ,sum(metrics_conversions) as conversions
   ,safe_divide(sum(metrics_clicks),sum(metrics_impressions)) as ctr
   ,sum(metrics_cost_micros)/1000000 as cost_gbp
 FROM {{ ref('stg_src_airbyte__google_ads_campaign') }}
-group by 1,2
+group by 1,2,3
