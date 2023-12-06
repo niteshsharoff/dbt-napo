137f724d46bbcddd0e7e0538433dbf9c1e793fc2
Nitesh Sharoff <nitesh@growthrunner.com>
2023-12-06 17:32:17 +0000

    ga4 fact table with incrementality and pcw mapping

---
+++ dbt-napo/models/marts/digital_analytics/fct_ga4.sql
@@ -0,0 +1,98 @@
+{{config(
+    materialized='incremental',
+    partition_by={
+        'field':'event_date',
+        'granularity':'day',
+        'data_type':'date'
+    },
+    cluster_by=['event_name','user_id','ga_session_id','transaction_id'],
+    pre_hook='DELETE from {{this}} where event_date >= date_sub(current_date,INTERVAL 3 DAY)'
+)}}
+
+
+
+with base_ga4 as (
+    select 
+         parse_date('%Y%m%d',event_date) as event_date
+        ,timestamp_micros(event_timestamp) as event_timestamp
+        ,coalesce(max(user_id) over(partition by user_pseudo_id order by event_timestamp desc),user_pseudo_id) as user_id
+    --  ,max(user_id) over(partition by user_pseudo_id order by event_timestamp desc) as user_id
+        ,{{ga4_unnest('ga_session_id')}}
+        ,event_name
+        ,device.web_info.hostname
+        ,replace(split((select value.string_value from unnest(event_params) where key = 'page_location'),'?')[safe_offset(0)],'https://www.napo.pet','') as page_path
+        ,split((select value.string_value from unnest(event_params) where key = 'page_location'),'?')[safe_offset(1)] as query_params_raw
+        ,case
+                when traffic_source.name = '(organic)' and traffic_source.medium='cpc' and traffic_source.source = 'google' then struct(
+                'Paid Search' as name
+                , traffic_source.medium as medium
+                , traffic_source.source as source
+                )
+                else traffic_source 
+        end as traffic_source
+        ,collected_traffic_source
+        ,privacy_info.analytics_storage
+        ,privacy_info.ads_storage
+        ,{{ga4_unnest('quote_id')}}
+        ,{{ga4_unnest('policy_ids')}}
+        ,{{ga4_unnest('transaction_type')}}
+        ,{{ga4_unnest('price_monthly','policy_price_monthly')}}
+        ,{{ga4_unnest('price_annual','policy_price_annual')}}
+        ,{{ga4_unnest('currency')}}
+        ,{{ga4_unnest('referrer')}}
+    --  ,(select value.string_value from unnest(event_params) where key = 'page_location') as page_location
+        ,coalesce(ecommerce.transaction_id,(select coalesce(value.string_value,cast(value.int_value as string)) from unnest(event_params) where key = 'transaction_id')) as transaction_id
+    
+    from {{source('ga4','events')}}
+    {%-if is_incremental()%}
+    where _table_suffix >= format_date('%Y%m%d',date_sub(current_date(),INTERVAL 3 DAY))
+    {%-else%}
+    where _table_suffix >= '20230101'
+    {%-endif%}
+    and user_pseudo_id is not null
+),
+
+features as (
+select * 
+      ,first_value(if(event_name='page_view',page_path,null) ignore nulls) over(partition by user_id, ga_session_id order by event_timestamp) as landing_page_path
+      ,max(if(lower(query_params_raw) like '%ttclid%',true,null)) over(partition by user_id,ga_session_id)as is_tiktok
+      ,max(if(lower(query_params_raw) like '%fbclid%',true,null)) over(partition by user_id,ga_session_id) as is_facebook
+      ,max(if(lower(query_params_raw) like any ('%gclid%','%wbraid%','gbraid'),true,null)) over(partition by user_id,ga_session_id) is_gads
+      ,max(if(lower(query_params_raw) like '%msclkid%',true,null)) over(partition by user_id,ga_session_id) as is_bing
+      ,max(if(lower(page_path) like '%inbound/%',true,null)) over(partition by user_id,ga_session_id) as is_pcw
+      ,max(REGEXP_EXTRACT(page_path, r'inbound/([^/]+).*')) over(partition by user_id,ga_session_id) AS pcw_raw
+      ,row_number() over(partition by user_id, ga_session_id order by event_timestamp asc) as event_no
+from base_ga4
+)
+select 
+     a.event_no
+    ,a.event_date
+    ,a.event_timestamp
+    ,a.user_id
+    ,a.ga_session_id
+    ,a.event_name
+    ,a.hostname
+    ,a.page_path
+    ,a.query_params_raw
+    ,a.landing_page_path
+    ,a.analytics_storage
+    ,a.quote_id
+    ,a.policy_ids
+    ,a.transaction_id
+    ,a.transaction_type
+    ,a.currency
+    ,a.policy_price_monthly
+    ,a.policy_price_annual
+    ,a.is_tiktok
+    ,a.is_facebook
+    ,a.is_gads
+    ,a.is_bing
+    ,a.is_pcw
+    ,a.referrer
+    ,lower(b.pcw_name) as pcw_name
+    ,a.traffic_source
+    ,a.collected_traffic_source
+from features a
+left join {{ref("lookup_quote_pcw_mapping")}} b
+on a.pcw_raw = b.quote_code_name
+--order by user_id, event_timestamp asc
