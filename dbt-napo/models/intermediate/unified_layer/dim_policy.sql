bebeedcb4dc820b596fc46476f115880a6443c10
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2023-12-07 16:24:46 +0000

    Address review comments for policy dimension

--- dbt-napo/models/intermediate/unified_layer/dim_policy.sql
+++ dbt-napo/models/intermediate/unified_layer/dim_policy.sql
@@ -44,56 +44,157 @@ with
         left join customer_uuid cu on policy.customer_id = cu.customer_id
         left join pet_uuid pe on policy.pet_id = pe.pet_id
     ),
-    compute_row_hash as (
+    has_row_changed as (
         select
             *,
-            farm_fingerprint(
-                concat(
-                    policy_id,
-                    quote_uuid,
-                    customer_uuid,
-                    pet_uuid,
-                    policy_uuid,
-                    reference_number,
-                    coalesce(quote_source_reference, ''),
-                    payment_plan_type,
-                    retail_price,
-                    coalesce(notes, ''),
-                    accident_cover_start_date,
-                    start_date,
-                    end_date,
-                    coalesce(cast(cancel_date as string), ''),
-                    coalesce(cancel_detail, ''),
-                    coalesce(cancel_reason, ''),
-                    created_date,
-                    coalesce(cast(sold_at as string), ''),
-                    coalesce(cast(cancelled_at as string), ''),
-                    coalesce(cast(reinstated_at as string), ''),
-                    coalesce(change_reason, '')
-                )
-            ) as row_hash
+            case
+                when
+                    policy_id != lag(policy_id) over (
+                        partition by policy_uuid order by effective_from
+                    )
+                then 1
+                when
+                    quote_uuid != lag(quote_uuid) over (
+                        partition by policy_uuid order by effective_from
+                    )
+                then 1
+                when
+                    customer_uuid != lag(customer_uuid) over (
+                        partition by policy_uuid order by effective_from
+                    )
+                then 1
+                when
+                    pet_uuid != lag(pet_uuid) over (
+                        partition by policy_uuid order by effective_from
+                    )
+                then 1
+                when
+                    policy_uuid != lag(policy_uuid) over (
+                        partition by policy_uuid order by effective_from
+                    )
+                then 1
+                when
+                    reference_number != lag(reference_number) over (
+                        partition by policy_uuid order by effective_from
+                    )
+                then 1
+                when
+                    coalesce(quote_source_reference, '') != coalesce(
+                        lag(quote_source_reference) over (
+                            partition by policy_uuid order by effective_from
+                        ),
+                        ''
+                    )
+                then 1
+                when
+                    payment_plan_type != lag(payment_plan_type) over (
+                        partition by policy_uuid order by effective_from
+                    )
+                then 1
+                when
+                    retail_price != lag(retail_price) over (
+                        partition by policy_uuid order by effective_from
+                    )
+                then 1
+                when
+                    coalesce(notes, '') != coalesce(
+                        lag(notes) over (
+                            partition by policy_uuid order by effective_from
+                        ),
+                        ''
+                    )
+                then 1
+                when
+                    accident_cover_start_date != lag(accident_cover_start_date) over (
+                        partition by policy_uuid order by effective_from
+                    )
+                then 1
+                when
+                    start_date != lag(start_date) over (
+                        partition by policy_uuid order by effective_from
+                    )
+                then 1
+                when
+                    end_date != lag(end_date) over (
+                        partition by policy_uuid order by effective_from
+                    )
+                then 1
+                when
+                    coalesce(cast(cancel_date as string), '') != coalesce(
+                        lag(cast(cancel_date as string)) over (
+                            partition by policy_uuid order by effective_from
+                        ),
+                        ''
+                    )
+                then 1
+                when
+                    coalesce(cancel_detail, '') != coalesce(
+                        lag(cancel_detail) over (
+                            partition by policy_uuid order by effective_from
+                        ),
+                        ''
+                    )
+                then 1
+                when
+                    coalesce(cancel_reason, '') != coalesce(
+                        lag(cancel_reason) over (
+                            partition by policy_uuid order by effective_from
+                        ),
+                        ''
+                    )
+                then 1
+                when
+                    created_date != lag(created_date) over (
+                        partition by policy_uuid order by effective_from
+                    )
+                then 1
+                when
+                    coalesce(cast(sold_at as string), '') != coalesce(
+                        lag(cast(sold_at as string)) over (
+                            partition by policy_uuid order by effective_from
+                        ),
+                        ''
+                    )
+                then 1
+                when
+                    coalesce(cast(cancelled_at as string), '') != coalesce(
+                        lag(cast(cancelled_at as string)) over (
+                            partition by policy_uuid order by effective_from
+                        ),
+                        ''
+                    )
+                then 1
+                when
+                    coalesce(cast(reinstated_at as string), '') != coalesce(
+                        lag(cast(reinstated_at as string)) over (
+                            partition by policy_uuid order by effective_from
+                        ),
+                        ''
+                    )
+                then 1
+                when
+                    coalesce(change_reason, '') != coalesce(
+                        lag(change_reason) over (
+                            partition by policy_uuid order by effective_from
+                        ),
+                        ''
+                    )
+                then 1
+                else 0
+            end as row_changed
         from policy_scd
     ),
-    group_changes as (
-        select
-            *,
-            -- identify rows that have changed
-            coalesce(
-                row_hash <> lag(row_hash) over (
-                    partition by policy_uuid order by effective_from
-                ),
-                true
-            ) as has_changed
-        from compute_row_hash
-    ),
-    assign_group_id as (
+    assign_grp_id as (
         select
             *,
             -- assign changes to buckets
-            sum(cast(has_changed as integer)) over (
-                partition by policy_uuid order by effective_from
+            coalesce(
+                sum(row_changed) over (
+                    partition by policy_uuid order by effective_from
+                ),
+                0
             ) as grp_id
-        from group_changes
+        from has_row_changed
     ),
     merge_duplicates as (
         select
@@ -106,7 +207,7 @@ with
             quote_source_reference,
             payment_plan_type,
             retail_price,
-            notes,  -- 10
+            notes,
             accident_cover_start_date,
             start_date,
             end_date,
@@ -116,35 +217,35 @@ with
             created_date,
             sold_at,
             cancelled_at,
-            reinstated_at,  -- 20
+            reinstated_at,
             change_reason,
             product_id,
             min(effective_from) as effective_from,
             max(effective_to) as effective_to
-        from assign_group_id
+        from assign_grp_id
         group by
-            1,
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9,
-            10,
-            11,
-            12,
-            13,
-            14,
-            15,
-            16,
-            17,
-            18,
-            19,
-            20,
-            21,
-            22,
+            policy_id,
+            policy_uuid,
+            quote_uuid,
+            customer_uuid,
+            pet_uuid,
+            reference_number,
+            quote_source_reference,
+            payment_plan_type,
+            retail_price,
+            notes,
+            accident_cover_start_date,
+            start_date,
+            end_date,
+            cancel_date,
+            cancel_detail,
+            cancel_reason,
+            created_date,
+            sold_at,
+            cancelled_at,
+            reinstated_at,
+            change_reason,
+            product_id,
             grp_id
         order by policy_uuid, effective_from
     ),
