e29213220f7ef58cc95a228cd9a816f02b92f5b8
Nitesh Sharoff <nitesh@growthrunner.com>
2023-12-12 11:44:52 +0000

    modified agg function to allow rename value input

--- dbt-napo/macros/aggregate_count_sum.sql
+++ dbt-napo/macros/aggregate_count_sum.sql
@@ -1,7 +1,10 @@
-{% macro agg(value,type='sum') %}
+{% macro agg(value,renamed_value=None,type='sum') %}
+  {%if renamed_value==None%}
+    {%set renamed_value = value%}
+  {%endif%}
   {%if type=='sum'%}
     sum(cast({{value}} as numeric)) as {{value}}
   {% elif type=='count' %}
-    count({{value}}) as {{value}}
+    count({{value}}) as {{renamed_value}}
   {% endif %}
 {% endmacro %}
