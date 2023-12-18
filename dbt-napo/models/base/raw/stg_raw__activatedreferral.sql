984d1d317cc4c529396dfe66816aabe2115e6b37
Nitesh Sharoff <nitesh@growthrunner.com>
2023-12-18 14:31:37 +0000

    formatting - fix 2 more tables

--- dbt-napo/models/base/raw/stg_raw__activatedreferral.sql
+++ dbt-napo/models/base/raw/stg_raw__activatedreferral.sql
@@ -1,35 +1,32 @@
-with 
+with
 
-source as (
+    source as (select * from {{ source("raw", "activatedreferral") }}),
 
-    select * from {{ source('raw', 'activatedreferral') }}
+    renamed as (
 
-),
+        select
+            id,
+            referral_code,
+            email,
+            created_at,
+            used_at,
+            referee_id,
+            referral_policy_id,
+            rewarded,
+            validate_at,
+            referee_reward,
+            referrer_reward,
+            referee_reward_mailchimp_email_id,
+            referrer_reward_mailchimp_email_id,
+            reward_type,
+            uuid,
+            referee_wegift_order_id,
+            referrer_wegift_order_id,
+            run_date
 
-renamed as (
+        from source
 
-    select
-        id,
-        referral_code,
-        email,
-        created_at,
-        used_at,
-        referee_id,
-        referral_policy_id,
-        rewarded,
-        validate_at,
-        referee_reward,
-        referrer_reward,
-        referee_reward_mailchimp_email_id,
-        referrer_reward_mailchimp_email_id,
-        reward_type,
-        uuid,
-        referee_wegift_order_id,
-        referrer_wegift_order_id,
-        run_date
+    )
 
-    from source
-
-)
-
-select * from renamed
+select *
+from renamed
--- dbt-napo/models/intermediate/int_renewal_quote.sql
+++ dbt-napo/models/intermediate/int_renewal_quote.sql
@@ -1,8 +1,4 @@
-{{ config(
-    materialized="table",
-    tags=['daily','growth']
-
-)}}
+{{ config(materialized="table", tags=["daily", "growth"]) }}
 select *
 from {{ ref("int_quote") }}
 where quote_id in (select distinct quote_id from {{ ref("stg_raw__renewal") }})
