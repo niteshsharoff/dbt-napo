ad5540ca871e60b0ac72ed6b040726693c148c39
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2024-01-03 13:42:16 +0000

    [GELR] Use clickup claim snapshot from the 2023-01-02

--- dbt-napo/models/intermediate/underwriter/int_underwriter__claim_snapshot_2023_12.sql
+++ dbt-napo/models/intermediate/underwriter/int_underwriter__claim_snapshot_2023_12.sql
@@ -2,4 +2,4 @@
 
 select *
 from {{ ref("int_underwriter__claim_snapshots") }}
-where snapshot_date = date(2024, 1, 1) and claim_received_date < date(2024, 1, 1)
+where snapshot_date = date(2024, 1, 2) and claim_received_date < date(2024, 1, 1)
