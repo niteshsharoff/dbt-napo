06cee857345c55fc731e48b0d7740cc49455e127
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2024-02-02 11:14:26 +0000

    Add engineering mart to monitor big query costs

--- dbt-napo/models/marts/engineering_metrics/big_query/big_query_job_usage.sql
+++ dbt-napo/models/marts/engineering_metrics/big_query/big_query_job_usage.sql
@@ -1,5 +1,5 @@
 with
-    query_metadata as (
+    big_query_job_usage as (
         select
             project_id,
             job_type,
@@ -12,20 +12,24 @@ with
                 destination_table.table_id
             ) as destination_table,
             total_bytes_processed / 1024 / 1024 / 1024 as gb_processed,
-            -- See: https://cloud.google.com/bigquery/pricing#queries, 6.25 USD per TB
-            -- queried
-            total_bytes_billed / 1024 / 1024 / 1024 / 1024 * 6.25 as job_cost_usd,
+            total_bytes_billed / 1024 / 1024 / 1024 / 1024 * 6.5 as job_cost_usd,
             timestamp_diff(end_time, start_time, second) as duration,
-            -- , query
+            query,
             date(creation_time) as created_at,
             start_time,
             end_time,
             total_bytes_processed,
             total_bytes_billed,
-            total_slot_ms,
-            cache_hit
-        from `region-eu`.information_schema.jobs_by_project jobs
+            total_slot_ms
+        -- fmt: off
+        from `region-eu`.INFORMATION_SCHEMA.JOBS_BY_PROJECT jobs
+        -- fmt: on
     )
-select *
-from query_metadata
-order by total_bytes_billed desc
+select
+    created_at,
+    destination_table,
+    sum(job_cost_usd) as job_cost_usd,
+    sum(gb_processed) as gb_processed
+from big_query_job_usage
+group by 1, 2
+order by 1 desc, 3 desc
