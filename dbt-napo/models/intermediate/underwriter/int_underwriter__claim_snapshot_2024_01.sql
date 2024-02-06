9214684a0c329d8cfb6a9742b911c3d1a8b17356
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2024-02-06 14:55:40 +0000

    Update January claim snapshot to use latest successful clickup snpashot

--- dbt-napo/models/intermediate/underwriter/int_underwriter__claim_snapshot_2024_01.sql
+++ dbt-napo/models/intermediate/underwriter/int_underwriter__claim_snapshot_2024_01.sql
@@ -2,4 +2,4 @@
 
 select *
 from {{ ref("int_underwriter__claim_snapshots") }}
-where snapshot_date = date(2024, 1, 31) and claim_received_date < date(2024, 2, 1)
+where snapshot_date = date(2024, 2, 6) and claim_received_date < date(2024, 2, 1)
