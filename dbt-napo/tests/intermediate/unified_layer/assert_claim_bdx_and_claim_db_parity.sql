fd079564044c0a9c56eaf7cfbd17eccf68c4cbba
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2024-02-20 16:38:11 +0000

    Add claim_bdx and claim_db parity test cases

--- dbt-napo/tests/intermediate/unified_layer/assert_claim_bdx_and_claim_db_parity.sql
+++ dbt-napo/tests/intermediate/unified_layer/assert_claim_bdx_and_claim_db_parity.sql
@@ -1,13 +1,23 @@
+{{ config(severity="warn") }}
+
+/*
+  GIVEN
+    a log of transactions from claim-service and a set of historical claim_bdxs
+  WHEN
+    we join them together on claim_reference
+  THEN
+    we expect no claims in the claim_bdx to be missing from the claim-service DB
+*/
 with
-    q1 as (
+    diff as (
         select
             claim_bdx.claim_reference as claim_bdx_reference,
             claim_db.claim_reference as claim_db_reference
-        from {{ ref("dim_claim_v1") }} claim_bdx
+        from {{ ref("dim_claim", v=1) }} claim_bdx
         full outer join
-            {{ ref("dim_claim_v2") }} claim_db
+            {{ ref("dim_claim", v=2) }} claim_db
             on claim_bdx.claim_reference = claim_db.claim_reference
     )
 select distinct *
-from q1
-where q1.claim_db_reference is null
+from diff
+where diff.claim_db_reference is null
