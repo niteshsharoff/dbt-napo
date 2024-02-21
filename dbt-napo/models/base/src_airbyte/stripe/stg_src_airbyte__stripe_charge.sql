719b182e2e24b63d497c04bcd7c551931d62f9c8
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2024-02-21 19:35:24 +0000

    Add refund and charge models from stripe

---
+++ dbt-napo/models/base/src_airbyte/stripe/stg_src_airbyte__stripe_charge.sql
@@ -0,0 +1,13 @@
+select
+    id as charge_id,
+    paid,
+    amount as charge_amount_mu,
+    status,
+    timestamp_seconds(created) as created_at,
+    timestamp_seconds(updated) as updated_at,
+    customer as stripe_customer_id,
+    json_extract_scalar(outcome, '$.reason') as reason,
+    refunded,
+    payment_intent as payment_intent_id,
+    failure_message
+from {{ source("src_airbyte", "stripe_charges") }}
---
+++ dbt-napo/models/base/src_airbyte/stripe/stg_src_airbyte__stripe_refund.sql
@@ -0,0 +1,13 @@
+with
+    refunds as (
+        select
+            id as stripe_refund_id,
+            payment_intent as payment_intent_id,
+            amount as refund_amount_mu,
+            reason as refund_reason,
+            status as refund_status,
+            timestamp_seconds(created) as refunded_at
+        from {{ source("src_airbyte", "stripe_refunds") }}
+    )
+select *
+from refunds
