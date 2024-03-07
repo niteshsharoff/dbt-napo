87a6df7a4940a2893bf0e03c1a21179bd02a180f
jeremiahmai <112863179+jeremiahmai@users.noreply.github.com>
2024-03-07 13:44:59 +0000

    Add schema docs and model configuration

--- dbt-napo/models/intermediate/quotes/int_quote_raw_requests.sql
+++ dbt-napo/models/intermediate/quotes/int_quote_raw_requests.sql
@@ -1,3 +1,5 @@
+{{ config(materialized="table", tags=["daily", "growth"]) }}
+
 with
     quote_requests as (
         select *
--- dbt-napo/models/marts/fct_quote_requests.sql
+++ dbt-napo/models/marts/fct_quote_requests.sql
@@ -2,7 +2,8 @@
 
 with
     stg_quote as (
-        select * except (customer), customer.* from {{ ref("stg_raw__quote_request") }}
+        select * except (customer), customer.*
+        from {{ ref("stg_raw__quote_request", v=1) }}
     )
 select *
 from stg_quote
