9a8858e05d6e827089a23dea6e20f7b648537050
Nitesh Sharoff <nitesh@growthrunner.com>
2024-03-18 21:50:43 +0000

    Tiktok - sql formatting

--- dbt-napo/models/base/src_airbyte/tiktok/stg_src_airbyte__tiktok_campaigns_reports_daily.sql
+++ dbt-napo/models/base/src_airbyte/tiktok/stg_src_airbyte__tiktok_campaigns_reports_daily.sql
@@ -1,21 +1,22 @@
-with source as (
-      select * from {{ source('src_airbyte', 'tiktok_campaigns_reports_daily') }}
-),
-renamed as (
-    select
-        {{ adapter.quote("_airbyte_raw_id") }},
-        {{ adapter.quote("_airbyte_extracted_at") }},
-        {{ adapter.quote("_airbyte_meta") }},
-        {{ adapter.quote("ad_id") }},
-        {{ adapter.quote("metrics") }},
-        {{ adapter.quote("adgroup_id") }},
-        {{ adapter.quote("dimensions") }},
-        {{ adapter.quote("campaign_id") }},
-        {{ adapter.quote("advertiser_id") }},
-        {{ adapter.quote("stat_time_day") }},
-        {{ adapter.quote("stat_time_hour") }}
+with
+    source as (
+        select * from {{ source("src_airbyte", "tiktok_campaigns_reports_daily") }}
+    ),
+    renamed as (
+        select
+            {{ adapter.quote("_airbyte_raw_id") }},
+            {{ adapter.quote("_airbyte_extracted_at") }},
+            {{ adapter.quote("_airbyte_meta") }},
+            {{ adapter.quote("ad_id") }},
+            {{ adapter.quote("metrics") }},
+            {{ adapter.quote("adgroup_id") }},
+            {{ adapter.quote("dimensions") }},
+            {{ adapter.quote("campaign_id") }},
+            {{ adapter.quote("advertiser_id") }},
+            {{ adapter.quote("stat_time_day") }},
+            {{ adapter.quote("stat_time_hour") }}
 
-    from source
-)
-select * from renamed
-  
+        from source
+    )
+select *
+from renamed
